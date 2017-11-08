429832484b1b2b83131ed536c3af41dd289f6344
David S. Jackson <deepbsd@yahoo.com>
2017-11-08 10:26:42 -0500

    trying to introduce some props and build a conditional header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,56 @@
 import React from 'react';
 
 
-import Nav from './nav';
+import NavItem from './navitem';
 
 
-export default function Header() {
-    return (
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loggedIn: true,
+            links: [{
+                link: "Register",
+                domain: "public"
+            },
+            {
+                link: "Login",
+                domain: "public"
+            },
+            {
+                link: "About",
+                domain: ["private", "public"]
+            },
+            {
+                link: "logout",
+                domain: "private"
+            },
+            {
+                link: "profile",
+                domain: "private"
+            }
+            ]
+        };
+    }
+
+
+    render() {
+
+            const links = this.state.links.map((link, index) =>
+                <NavItem key={index} {...link} />
+            );
+
+
+
+
+        return (
             <div className="">
-                <Nav />
+                {links}
             </div>
         );
+    }
 }
+
+
+
---
+++ src/components/navitem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './navitem.css';
+
+export default function NavItem(props) {
+    return (
+        <li>{props.link}</li>
+    );
+}
+
