4a0dbd74f128a20b8bc3d8e85efbb7fa9c57b7ff
David S. Jackson <deepbsd@yahoo.com>
2017-11-07 20:48:43 -0500

    added a mock header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './header.css';
+
+export default function Header() {
+    return (
+            <div className="header">
+                <ul>
+                    <li>one</li>
+                    <li>two</li>
+                    <li>three</li>
+                </ul>
+            </div>
+        );
+}
