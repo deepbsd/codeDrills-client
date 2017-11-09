037cf80e294521be740fee79751f585271fed5d5
David S. Jackson <deepbsd@yahoo.com>
2017-11-09 08:41:13 -0500

    updated css and js for app, header, landing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 import Landing from './components/landing';
@@ -11,13 +11,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Header className="header"/>
-          <img src={logo} className="App-logo" alt="logo" />
-          
+          <Header className="header"/>          
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Landing />
       </div>
     );
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import logo from '../logo.svg';
 
 import './landing.css';
 
@@ -7,7 +8,11 @@ import './landing.css';
 export default function Landing() {
     return (
             <div className="landing">
-                <h3>Landing Component</h3>
+                <img src={logo} className="App-logo" alt="logo" />
+                <h3>About Code Drills</h3>
+                <p>CodeDrills helps web developers practice for technical interviews. 
+                Here you can take quizzes about HTML, CSS, and many aspects of JS
+                    to help you prepare for technical interview questions.</p>
             </div>
         );
 }
