79e7a2896c7b7deb07e324cdeee90d40b1831799
David S. Jackson <deepbsd@yahoo.com>
2017-11-19 22:55:09 -0500

    made profile.js into a class with state including userData info

--- src/components/profile.js
+++ src/components/profile.js
@@ -4,23 +4,53 @@ import Footer from './footer';
 import './profile.css';
 
 
-export default function Profile() {
+export default class Profile extends React.Component {
 
-    return (
-        <div>
-            <div className="profile">
-                <img src="http://placekitten.com/g/200/200" alt="" />
-                <form>
-                    <input type="text" placeholder="username"></input><br/>
-                    <input type="text" placeholder="email"></input><br/>
-                    <input type="text" placeholder="password"></input>
-                </form>
-                <div className="profileData">
-     
+    constructor(props) {
+        super(props);
+
+        this.loggedIn = true;
+
+        this.state = {
+            loggedIn: this.loggedIn,
+            user: {
+                username: 'Joe',
+                firstName: 'Joe',
+                lastName: 'Blow',
+                password: 'sonorapass'
+            },
+            userData: {
+                numberOfQuizzes: 18,
+                totalQuestions: 180,
+                totalCorrect: 157,
+                jsQuestionsAnswered: 123,
+                jsQuestionsCorrect: 199,
+                htmlQuestionsAnswered: 45,
+                htmlQuestionsCorrect: 44,
+                cssQuestionsAnwswered: 12,
+                cssQuestionsCorrect: 12
+            }
+        };
+    }
+
+
+    render() {
+
+        return (
+            <div>
+                <div className="profile">
+                    <img src="http://placekitten.com/g/200/200" alt="" />
+                    <form>
+                        <input type="text" placeholder="username"></input><br/>
+                        <input type="text" placeholder="email"></input><br/>
+                        <input type="text" placeholder="password"></input>
+                    </form>
+                    <div className="profileData">
+         
+                    </div>
                 </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
-    );
-
+        );
+    }
 }
