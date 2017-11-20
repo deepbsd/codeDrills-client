c5fbbd02e8f6a3db44d1e32553a95d7de70548dd
David S. Jackson <deepbsd@yahoo.com>
2017-11-20 13:44:57 -0500

    okay, test should pass now

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-import Main from './components/main';
+import Main from './components/Main';
 
-import Header from './components/header';
+import Header from './components/Header';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        
+
         <header className="App-header">
 
-          <Header className="header" />          
-        
+          <Header className="header" />
+
         </header>
-        
+
         <Main />
-      
+
       </div>
     );
   }
