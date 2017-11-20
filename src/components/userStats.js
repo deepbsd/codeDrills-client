feaff4367aee95359e765c38064129f68f7bfe26
David S. Jackson <deepbsd@yahoo.com>
2017-11-20 11:28:42 -0500

    testing testing

--- src/components/userStats.js
+++ src/components/userStats.js
@@ -6,6 +6,7 @@ export default function Userstats(props) {
 
         <div className="stats">
             <table >
+                <th><td>Number of Questions</td><td>Total Questions</td><td>Total Correct</td></th>
                 <tr><td>{props.userData.numberOfQuizzes}</td>
                 <td>{props.userData.totalQuestions}</td>
                 <td>{props.userData.totalCorrect}</td></tr>
