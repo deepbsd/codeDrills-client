6dcbc71e37ac5058eac137fdd51dc9d8b1fb6a4d
David S. Jackson <deepbsd@yahoo.com>
2017-11-23 19:51:22 -0500

    removed thing.js

--- src/components/userStats.js
+++ src/components/userStats.js
@@ -6,11 +6,13 @@ export default function Userstats(props) {
     return (
 
         <div className="profile">
-            <table >
+            <table>
                 <th><td>Number of Questions</td><td>Total Questions</td><td>Total Correct</td></th>
-                <tr><td>{props.userData.numberOfQuizzes}</td>
-                <td>{props.userData.totalQuestions}</td>
-                <td>{props.userData.totalCorrect}</td></tr>
+                <tr>
+                  <td>{props.userData.numberOfQuizzes}</td>
+                  <td>{props.userData.totalQuestions}</td>
+                  <td>{props.userData.totalCorrect}</td>
+                </tr>
             </table>
         </div>
 
