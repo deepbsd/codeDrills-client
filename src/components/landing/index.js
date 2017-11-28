a4868bf91aa1b08eb945ec3f1ded986069cb1e55
David S. Jackson <deepbsd@yahoo.com>
2017-11-28 16:13:25 -0500

    added image to landing page

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -10,9 +10,13 @@ export default function Landing() {
             <div className="landing">
                 <img src={logo} className="App-logo" alt="logo" />
                 <h3>Welcome to Code Drills</h3>
-                <p>CodeDrills helps web developers practice for technical interviews.
-                Here you can take quizzes about HTML, CSS, and many aspects of JS
-                    to help you prepare for technical interview questions.</p>
+                <p>
+                  CodeDrills helps web developers practice for technical interviews.
+                  Here you can take quizzes about HTML, CSS, and many aspects of JS
+                  to help you prepare for technical interview questions. The
+                  interview is your bridge from where you are in your career to
+                  where you want to be.
+                </p>
             </div>
         );
 }
