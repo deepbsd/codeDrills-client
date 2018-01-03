6ad5ca82030b1a97dc31fc7252de7496ad3d9069
David S. Jackson <deepbsd@yahoo.com>
2018-01-03 17:31:51 -0500

    changed name for two components (to index.js) and also started work on updating global state object with currentUser data--that is not going so well right now...

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import Userstats from '../userStats/userStats';
-import Userdetails from '../userDetails/userDetails';
+import Userstats from '../userStats';
+import Userdetails from '../userDetails';
 import {BarChart, PieChart, PolarChart, LineChart, RadarChart} from '../chart';
 
 
