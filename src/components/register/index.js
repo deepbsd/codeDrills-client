5dfcff0622bd7d2feaf2822288e426c06209ddc8
David S. Jackson <deepbsd@yahoo.com>
2018-01-16 22:13:03 -0500

    styling registeration form as styled component--just starting

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import './register.css';
+import Style from './style';
 
 export default function Register() {
     return (
-        <form className="register">
+        <Style.form>
             First Name: <input type="text" name="first name" value="first name" />
             Last Name: <input type="text" name="last name" value="last name" />
             Username: <input type="text" name="username" value="username" />
             Password: <input type="text" name="password" value="password" />
             <input type="submit" value="submit" />
-        </form>
+        </Style.form>
     );
 }
---
+++ src/components/register/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Style = {
+  form: styled.form`
+    margin-right: 12px;
+  `
+}
+
+
+
+
+
+
+export default Style;
