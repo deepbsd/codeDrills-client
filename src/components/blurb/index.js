68379039806cf03a4327da942855d5cd0c8d4293
David S. Jackson <deepbsd@yahoo.com>
2018-01-22 13:27:41 -0500

    updating styling on blurb component

--- src/components/blurb/index.js
+++ src/components/blurb/index.js
@@ -8,7 +8,8 @@ export default function Blurb(props) {
 
     return (
       <Style.wrapper>
-          {props.title !== null ? <div><Style.head3>{props.title}</Style.head3> <Style.paragraph>{props.blurb}</Style.paragraph></div> : <Style.paragraph>{props.blurb}</Style.paragraph>}
+          {props.title !== null ? <div><Style.head3>{props.title}</Style.head3> <Style.paragraph>{props.blurb}</Style.paragraph></div>
+          : <Style.paragraph>{props.blurb}</Style.paragraph>}
       </Style.wrapper>
     );
 
