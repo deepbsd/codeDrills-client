2ae460f56f94e8b83b3af0761be512564517ab46
David S. Jackson <deepbsd@yahoo.com>
2018-01-29 11:55:32 -0500

    blah blah

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {loginUser} from '../../actions';
+import {loginUser, loadQuestions} from '../../actions';
 import NavItem from '../navitem';
 
 
