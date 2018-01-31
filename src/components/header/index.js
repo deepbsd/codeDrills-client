e6ddb1c11bc1db854ffacc046970e3ea5f6b38ce
Matthew Day <matt@matthewday.net>
2018-01-31 05:55:28 -0500

    Move navs from global state to local state

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,6 +8,47 @@ class Header extends React.Component {
     constructor(props) {
         super(props);
         this.testDispatch = this.testDispatch.bind(this);
+        
+        this.state = {
+          navs: [
+            {
+                link: "register",
+                href: "/register",
+                private: false,
+                public: true
+            },
+            {
+                link: "about",
+                href: "/about",
+                private: true,
+                public: true
+            },
+            {
+                link: "login",
+                href: "/login",
+                private: false,
+                public: true
+            },
+            {
+                link: "logout",
+                href: null,
+                private: true,
+                public: false
+            },
+            {
+                link: "profile",
+                href: "/profile",
+                private: true,
+                public: false
+            },
+            {
+                link: "start quiz",
+                href: "/startquiz",
+                private: true,
+                public: false
+            }
+          ]
+        }
     }
 
     // componentDidMount() {
@@ -23,7 +64,7 @@ class Header extends React.Component {
     render() {
         //let obj = JSON.parse(this.props.questions);
         console.log('Questions: ' + JSON.stringify(this.props.questions));
-        const links = this.props.navs.map((link, index) => {
+        const links = this.state.navs.map((link, index) => {
             if (this.props.loggedIn && link.private) {
               if (link.link === "logout") {
                 return (
@@ -55,7 +96,6 @@ class Header extends React.Component {
                 //<NavItem link="test" testDispatch={this.testDispatch} />
 const mapStateToProps = state => ({
     currentUser: state.currentUser,
-    navs: state.navs,
     loggedIn: state.loggedIn,
     questions: state.questions
 });
