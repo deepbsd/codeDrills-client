243620d2e1351a89e9daeb540ad325afdf1653cd
Matthew Day <matt@matthewday.net>
2018-01-31 06:14:46 -0500

    Convert user data to mock JSON

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,132 +1,13 @@
 import * as actions from '../actions';
 import update from 'immutability-helper';
 
+// these will need to be moved elsewhere
 import question5 from './img/question5.png';
 import question9 from './img/question9.png';
 import question16 from './img/question16.png';
 import question20 from './img/question20.png';
 
-
-
-const initialState = {
-      loggedIn: true,
-    questions: [],
-  currentQuiz: {
-        correct: [],
-        incorrect: [],
-        js: [],
-        js_right: [],
-        html: [],
-        html_right: [],
-        css: [],
-        css_right: [],
-        node: [],
-        node_right: [],
-        api: [],
-        api_right: [],
-        mongo: [],
-        mongo_right: []
-    },
-  missedQuestions: [],
-  correctQuestions: [],
-  currentUser: {
-      user: {
-          username: 'Joe',
-          firstName: 'Joe',
-          lastName: 'Blow',
-          email: 'joeblow@whatever.com',
-          password: 'sonorapass'
-      },
-      userData: {
-          missedQuestions: [],
-          numberOfQuizzes: 20,
-          totalQuestions: 200,
-          totalCorrect: 187,
-          jsQuestionsAnswered: 60,
-          jsQuestionsCorrect: 50,
-          cssQuestionsAnswered: 45,
-          cssQuestionsCorrect: 42,
-          htmlQuestionsAnswered: 35,
-          htmlQuestionsCorrect: 35,
-          nodeQuestionsAnswered: 30,
-          nodeQuestionsCorrect: 28,
-          apiQuestionsAnswered: 30,
-          apiQuestionsCorrect: 29,
-          mongoQuestionsAnswered: 5,
-          mongoQuestionsCorrect: 5
-      },
-      lastQuizData: {
-          totalQuestions: 10,
-          dateOfQuiz: new Date("2017-06-12T16:08:00"),
-          totalCorrect: 9,
-          timeOnQuiz: 1000340
-      },
-      missedMost: {
-          category: 'Vanilla Javascript',
-          moreThan3: [5, 18, 45],
-          neverCorrect: {
-              question: "What's the difference between 'let' and 'var'?",
-              lastAnswer: '\'let\' is not a constant',
-              choices: ["'let' is ES5",
-              "'var' is ES5",
-              "'let is ES6'",
-              "'let' pollutes the parent namespace",
-              "'var' is not what the cool kids do anymore"],
-              correctAnswer: "'let' is ES6"
-          }
-      },
-      chartData: {
-        labels: ["JS Questions Answered", "JS Questions Correct", "CSS Questions Answered", "CSS Questions Correct", "HTML Questions Answered", "HTML Questions Correct", "Node Questions Answered","Node Questions Correct","API Questions Answered","API Questions Correct"],
-        datasets: [
-          {
-            label: "Questions Correctly Answered",
-            data: [60, 50, 45, 42, 35, 35, 30, 28, 30, 29],
-            backgroundColor: ['purple', 'thistle', 'orange', 'yellow', '#0033ff', 'cyan', 'crimson', '#ff0066', 'green', 'lime']
-          }
-        ]
-      },
-      radarData: {
-        labels: ["JS Pct","CSS Pct", "HTML Pct","Node Pct","API Pct"],
-        datasets: [
-          {
-            label: "Overall Percentages",
-            backgroundColor: 'rgba(255, 204, 204,0.7)',
-            borderColor: 'rgba(255,99,132,1)',
-            pointBackgroundColor: 'rgba(255,99,132,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(255,99,132,1)',
-            data: [50/60, 42/45, 35/35, 28/30, 29/30],
-          },
-          {
-            label: "Latest Percentages",
-            backgroundColor: 'rgba(255, 200, 132,0.7)',
-            borderColor: 'rgba(255,99,132,1)',
-            pointBackgroundColor: 'rgba(255,99,132,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(255,99,132,1)',
-            data: [58/60, 45/45, 34/35, 29/30, 30/30],
-          }
-        ]
-      },
-      polarData: {
-        labels: ["JS Pct","CSS Pct", "HTML Pct","Node Pct","API Pct"],
-        datasets: [
-          {
-            label: "Questions Correctly Answered",
-            backgroundColor: ['purple', 'thistle', 'orange', 'yellow', '#0033ff', 'cyan', 'crimson', '#ff0066', 'green', 'lime'],
-            borderColor: 'rgba(255,99,132,1)',
-            pointBackgroundColor: 'rgba(255,99,132,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(255,99,132,1)',
-            data: [50/60, 42/45, 35/35, 28/30, 29/30],
-          }
-        ]
-      }
-  }
-};
+const initialState = { };
 
 export const reducer = (state=initialState, action) => {
   if (action.type === actions.LOGIN_USER) {
