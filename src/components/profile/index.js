5625b4c258d911a5353c2f4b12506acf33f68328
David S. Jackson <deepbsd@yahoo.com>
2018-02-09 08:06:40 -0500

    replaced userstats and usercharts with 6 pie charts whose data are now back in state--they may even stay there now that I'm rethinking this

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -13,6 +13,58 @@ export class Profile extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+          htmlChartData: {
+            "displayName": "HTML Questions",
+            "labels": ["HTML Questions Answered", "HTML Questions Correct"],
+            "datasets": [{
+              "label": "Questions Correctly Answered",
+              "data": [60, 50],
+              "backgroundColor": ["purple", "thistle"]
+            }]
+          },
+          cssChartData: {
+              "labels": ["CSS Questions Answered", "CSS Questions Correct"],
+              "datasets": [{
+                "label": "Questions Correctly Answered",
+                "data": [45, 42],
+                "backgroundColor": ["orange", "yellow"]
+              }]
+          },
+          jsChartData: {
+              "labels": ["JS Questions Answered", "JS Questions Correct"],
+              "datasets": [{
+                "label": "Questions Correctly Answered",
+                "data": [35, 35],
+                "backgroundColor": ["#0033ff", "cyan"]
+              }]
+          },
+          nodeChartData: {
+              "labels": ["Node Questions Answered", "Node Questions Correct"],
+              "datasets": [{
+                "label": "Questions Correctly Answered",
+                "data": [30, 28],
+                "backgroundColor": ["crimson", "#ff0066"]
+              }]
+          },
+          apiChartData: {
+              "labels": ["API Questions Answered", "API Questions Correct"],
+              "datasets": [{
+                "label": "Questions Correctly Answered",
+                "data": [30, 29],
+                "backgroundColor": ["green", "lime"]
+              }]
+          },
+          mongoChartData: {
+              "labels": ["Mongo Questions Answered", "Mongo Questions Correct"],
+              "datasets": [{
+                "label": "Questions Correctly Answered",
+                "data": [30, 27],
+                "backgroundColor": ["#f48f42", "#f46242"]
+              }]
+          }
+        }
     }
 
     componentWillMount() {
@@ -32,9 +84,15 @@ export class Profile extends React.Component {
                 {/*
                 <Userstats className="profile.css" userData={currentUser.userData}
                 lastQuiz={currentUser.lastQuizData} missedMost={currentUser.missedMost}  />
-                */}
                 <BarChart barChartData={currentUser.chartData} />
                 <PieChart pieChartData={currentUser.chartData} />
+                */}
+                <PieChart pieChartData={this.state.htmlChartData} />
+                <PieChart pieChartData={this.state.cssChartData} />
+                <PieChart pieChartData={this.state.jsChartData} />
+                <PieChart pieChartData={this.state.nodeChartData} />
+                <PieChart pieChartData={this.state.apiChartData} />
+                <PieChart pieChartData={this.state.mongoChartData} />
                 {/*
                 <RadarChart radarChartData={currentUser.radarData} />
                 <PolarChart polarChartData={currentUser.polarData} />
