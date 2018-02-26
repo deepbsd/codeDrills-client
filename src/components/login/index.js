8f60f63d38110bb7741f3b3456a878e2459637f8
David S. Jackson <deepbsd@yahoo.com>
2018-02-26 10:22:29 -0500

    added wrapper for login page so if user is logged in the login page redirects straight to the profile page

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,56 +1,26 @@
 import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
-import Input from '../register/input';
-import {login} from '../../actions/auth';
-import {required, nonEmpty} from '../../validators';
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
 
-export class LoginForm extends React.Component {
-    onSubmit(values) {
-        return this.props.dispatch(login(values.username, values.password));
+import LoginForm from './login-form';
+
+export function LandingPage(props) {
+    // If we are logged in redirect straight to the user's dashboard
+    if (props.loggedIn) {
+        return <Redirect to="/profile" />;
     }
 
-    render() {
-        let error;
-        if (this.props.error) {
-            error = (
-                <div className="form-error" aria-live="polite">
-                    {this.props.error}
-                </div>
-            );
-        }
-        console.log("LOGIN Token: ",this.props);
-        return (
-            <form
-                className="login-form"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                {error}
-                <label htmlFor="username">Username</label>
-                <Field
-                    component={Input}
-                    type="text"
-                    name="username"
-                    id="username"
-                    validate={[required, nonEmpty]}
-                />
-                <label htmlFor="password">Password</label>
-                <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    id="password"
-                    validate={[required, nonEmpty]}
-                />
-                <button disabled={this.props.pristine || this.props.submitting}>
-                    Log in
-                </button>
-            </form>
-        );
-    }
+    return (
+        <div className="home">
+            <h2>Welcome to Foo App</h2>
+            <LoginForm />
+            <Link to="/register">Register</Link>
+        </div>
+    );
 }
 
-export default reduxForm({
-    form: 'login',
-    onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
-})(LoginForm);
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(LandingPage);
