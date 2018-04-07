ba5dde7c8c6eb1cf0f3ec3564fb789d5370bbb35
David S. Jackson <deepbsd@yahoo.com>
2018-04-07 08:42:40 -0400

    still working on problem

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -92,8 +92,9 @@ export class Profile extends React.Component {
   // User Auth changes a lot here...  Have to adapt...
 
 const mapStateToProps = state => {
+  const whatever = state;
   const authorizedUser = state.auth;
-  console.log("Profile--STATEOBJ: ",state);
+  console.log("Profile--STATEOBJ: ",whatever);
   return {
     currentUser: state.reducer.currentUser,
     username: state.auth.currentUser,
