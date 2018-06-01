cd0b9ff0aac17b88e1a79813b8234f6a321e4cea
David S. Jackson <deepbsd@yahoo.com>
2018-06-01 08:32:48 -0400

    added react-avatar module for profile page

--- src/components/userDetails/index.js
+++ src/components/userDetails/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 // import '../profile/profile.css';
 import Style from '../profile/style.js';
+import Avatar from 'react-avatar';
 
 export default function Userdetails(props) {
 
     return (
 
         <Style.profile className="profile">
-            <Style.img src="http://placekitten.com/g/400/400" alt="userKitty" />
+        
+            <Avatar email="deepbsd@yahoo.com" twitterHandle="deepbsd"  size="400 "round={true} />
             <form>
                 <Style.input type="text" placeholder="first name" defaultValue={props.user.firstName}></Style.input><br/>
                 <Style.input type="text" placeholder="last name" defaultValue={props.user.lastName}></Style.input><br/>
@@ -19,4 +21,7 @@ export default function Userdetails(props) {
     );
 
 }
+
+
+//  <Style.img src="http://placekitten.com/g/400/400" alt="userKitty" />
 //  <Style.input type="text" placeholder="email" defaultValue={props.user.email}></Style.input><br/>
