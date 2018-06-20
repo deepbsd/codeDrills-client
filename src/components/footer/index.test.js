266792e398906212f793e120bd42636297961cdf
David S. Jackson <deepbsd@yahoo.com>
2018-06-19 23:12:40 -0400

    working on tests

---
+++ src/components/footer/index.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Style from './style';
+import Footer from './index';
+import {shallow, mount} from 'enzyme';
+
+
+describe('<Footer/>', () => {
+         
+
+   it('Renders without crashing', () => {
+       shallow(<Footer />);
+                   
+   });
+
+
+});
