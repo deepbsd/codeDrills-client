898d27550e8e72d07858ef211e96f23e0068a300
David S. Jackson <deepbsd@yahoo.com>
2018-07-18 10:55:37 -0400

    updated npm deps, added async to App test--working on a better solution using promises

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import moxios from 'moxios';
 import {shallow, mount, render} from 'enzyme';
 
-import {fetchQuestions} from './actions';
 import {BrowserRouter as Router } from 'react-router-dom';
 import {connect} from 'react-redux';
 import Main from './components/main';
@@ -36,78 +34,36 @@ describe('App Parent component ', () => {
         expect(footer.length).toBe(1);
     });
 
-    const questions =  {
-            number: 1,
-            question: "Why is React.js so hard?",
-            category: "React.js",
-            assetUrl: "http://localhost/images",
-            assetType: "image",
-            answers: [1,2,3]
-        };
-
-
-    beforeEach(() => {
-        moxios.install();
-    });
-
-    afterEach(() => {
-        moxios.uninstall();
-    });
-
-    it('updates store with questions on being mounted', () => {
-        let store = storeFactory({});
-        const length = Math.floor((Math.random()*40)+1);
-        const questionsArray = Array(length).fill(questions)
-
-        console.log(questionsArray.length)
-
-        moxios.wait(() => {
-         const request = moxios.requests.mostRecent();
-         request.respondWith({
-             status: 200,
-             response: questionsArray,
-         })
-        })
-        
-        //return store.dispatch(fetchQuestions())
-        //    .then(() => {
-        //        const newState = store.getState();
-        //        console.log(newState)
-        //        expect(newState.questions.length).toEqual(questionsArray.length)
-        //    });
-    });
 });
 
+describe('Component dispatches fetchQuestions on mount', () => {
+
+    let wrapper, store;
+
+    const fetchQuestionsMock = jest.fn();
+    
+	let initialState = {
+	  fetchQuestions: fetchQuestionsMock
+    }
+	
+	store = storeFactory(initialState);
+    wrapper = shallow(<App store={store} />);
+    
+
+    it('Dispatches fetchQuestions on mount', () => {
+      wrapper.instance().componentDidMount();
+      const fetchQuestionsCallLength = fetchQuestionsMock.mock.calls.length;
+      
+	  setTimeout(() => {
+	    expect(fetchQuestionsCallLength).toBe(1);
+	  }, 500);
+	});
+
+});
+	
 
 
 
 
-//describe('getSecretWord action creator', () => {
-//    beforeEach(() => {
-//        moxios.install();
-//    });
-//
-//    afterEach(() => {
-//        moxios.uninstall();
-//    });
-//
-//    it('correctly adds the secret word to state', () => {
-//       const secretWord = 'party';
-//       const store = storeFactory();
-//
-//       moxios.wait(() => {
-//           const request = moxios.requests.mostRecent();
-//           request.respondWith({
-//               status: 200,
-//               response: secretWord,
-//           });
-//       });
-//
-//       return store.dispatch(getSecretWord())
-//           .then(() => {
-//               const newState = store.getState();
-//               expect(newState.secretWord).toBe(secretWord);
-//           });
-//
-//    });
-//});
+
+
