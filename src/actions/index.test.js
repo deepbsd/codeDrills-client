47fc0b1b4d5f3019d57abde7e1dc1471094c5a80
David S. Jackson <deepbsd@yahoo.com>
2018-07-26 23:05:13 -0400

    adding test skeleton for action creator

---
+++ src/actions/index.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {shallow, mount, render} from 'enzyme';
+import {API_BASE_URL} from '../config';
+
+import {fetchUserData} from './index';
+
+
+
+describe('fetchUserData action creator', () => {
+    it('successfully connects to an endpoint', () => {
+
+    })
+
+    it('dispatches fetchUserDataSuccess', () => {
+
+    })
+})
+
+describe('fetchUserDataSuccess action creator', () => {
+    it('sucessfully updates global state with userdata', () => {
+
+    })
+    it('connects successfully to endpoint', () => {
+
+    })
+})
+
