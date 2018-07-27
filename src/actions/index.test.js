742d0af9c7c6176069d44a67fce744ed0c3b0f4a
David S. Jackson <deepbsd@yahoo.com>
2018-07-26 23:24:37 -0400

    updating action creator test with skeleton tests

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -25,3 +25,66 @@ describe('fetchUserDataSuccess action creator', () => {
     })
 })
 
+describe('createNewUserData', () => {
+    it('action successfully returns ok for POST request', () => {
+
+    })
+    it('successfully posts data to new mock user', () => {
+
+    })
+})
+
+describe('updateUserDb', () => {
+    it('action successfully connects to userdata endpoint', () => {
+
+    })
+    it('successfully calls the updateUserDataDb action', () => {
+
+    })
+})
+
+describe('updateUserDataDb success action', () => {
+    it('successfully updates global state with latest userdata from db', () => {
+
+    })
+})
+
+describe('resetUserDataDbSucess', () => {
+    it('global state status for reset success is reset from false to true', () => {
+
+    })
+})
+
+describe('resetLastQuizData', () => {
+    it('Last quiz data gets reset when a new quiz starts', () => {
+
+    })
+})
+
+describe('addCurrentUserToState', () => {
+    it('new User Object gets added to global state when user logs in', () => {
+
+    })
+})
+
+describe('fetchQuestions', () => {
+    it('gets called when user logs in and app starts', () => {
+
+    })
+    it('sucessfully calls loadQuestionSuccess', () => {
+
+    })
+})
+
+describe('loadQuestionsSucess', () => {
+    it('successfully updates global state with questions', () => {
+
+    })
+})
+
+describe('updateCurrent', () => {
+    it('gets called for each question to update whether answer is right or wrong', () => {
+
+    })
+
+})
