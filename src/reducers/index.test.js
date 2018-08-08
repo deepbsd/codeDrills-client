727882c60d25974d1dceec1693a9036630fa708b
David S. Jackson <deepbsd@yahoo.com>
2018-08-08 18:36:06 -0400

    starting tests on reducer, added fetch-mock with npm

---
+++ src/reducers/index.test.js
@@ -0,0 +1,21 @@
+import {
+	RESET_LAST_QUIZ_DATA,
+	resetLastQuizData,
+	UPDATE_CURRENT,
+	updateCurrent,
+	RESET_USERDATA_DB_SUCCESS,
+	resetUserDataDbSuccess,
+	FETCH_USER_DATA_SUCCESS,
+	fetchUserDataSuccess,
+	ADD_CURRENT_USER_TO_STATE,
+	addCurrentUserToState
+
+}  from './index';
+
+
+
+describe('CHECK_QUESTION', () => {
+  it('retrieves 40 questions', () => {
+    
+  })
+})
